7ef758f6ab81e7c9770b32335bf4f98944e27d38
CHIPCHILINK4 <valevkirill18082006@gmail.com>
2026-09-19 15:33:24 +0700

    Update main.cpp
    сделано, но есть баг синхронизации потоков, возможно его можно отловить, выражается так. Новое значение даты создается, но в этот момент выводится данные после  одного потребителя и они накладываются между собой

--- lab_1_Romanov_932424/main.cpp
+++ lab_1_Romanov_932424/main.cpp
@@ -5,11 +5,11 @@
 
 
 using namespace std;
-
+int COUNT = 5;
 
 
 class Data_for_needs{
-    int* k = 0;
+    int* k = nullptr;
 
 public:
     Data_for_needs(int d){
@@ -18,52 +18,101 @@ public:
     ~Data_for_needs(){
         delete k;
     }
+    int get_val(){
+        return *k;
+    }
+    void set_val(int d){
+        *k = d;
+    }
 
 };
 
-struct Monitor_for_prog
+class Monitor_for_prog
 {
-    Data_for_needs* dat; 
+    Data_for_needs* dat = nullptr; 
     pthread_mutex_t mtx;
     pthread_cond_t cv;
     bool flag = false;
-};
+public:
+    Monitor_for_prog() {
+        pthread_mutex_init(&mtx, nullptr);
+        pthread_cond_init(&cv, nullptr);
+    }
+    ~Monitor_for_prog() {
+        pthread_mutex_destroy(&mtx);
+        pthread_cond_destroy(&cv);
+    }
+    void provide(int n){
+        pthread_mutex_lock(&mtx);
+        cout << "P_lock"<<endl;
+
+        sleep(1);
+        while (flag) {
+            pthread_mutex_unlock(&mtx);
+            sleep(1); 
+            pthread_mutex_lock(&mtx);
+        }
+        delete dat;
+        dat = new Data_for_needs(n);
+        flag = true;
+        cout << "P_new_event"<<endl;
+
+        cout << "P_signal"<<endl;
+        pthread_cond_signal(&cv);
+
+        cout << "P_unlock"<<endl<<endl;
+        pthread_mutex_unlock(&mtx);
+
+    }
+    int consume() {
+        pthread_mutex_lock(&mtx);
+        cout << "c_lock"<<endl;
+        while (!flag) {
+            cout << "c wait, flag=0"<<endl;
+            pthread_cond_wait(&cv, &mtx);
+        }
+        sleep(1);
+        int result = dat->get_val();
+        cout << "get val: " << result<<endl;
+        flag = false;
+        cout << "c_unlock"<<endl<<endl;
+        pthread_mutex_unlock(&mtx);
+        return result;
+    }
+
+};  
+static Monitor_for_prog monitor;
 
 //поставщик
 void* producer(void* arg){
-
+    for (size_t i = 0; i < COUNT; i++)
+    {
+        monitor.provide(i*10);
+    }
+    return nullptr;
 }
 
 //потребитель
 void* consumer(void* arg){
-
+    for (size_t i = 0; i < COUNT; i++)
+    {
+        int result = monitor.consume();
+        cout << "-----------" << result << "-----------"<<endl<<endl;
+    }
+    return nullptr;
 }
 
 
-
-Monitor_for_prog monitor;
-
 int main() {
-    //первые базовые переменные
-    pthread_mutex_init(&monitor.mtx, nullptr);
-    pthread_cond_init(&monitor.cv, nullptr);
-
-    monitor.flag = false;
-    monitor.dat = nullptr;
-    
     //потоки
     pthread_t  producer_thread, consumer_thread;
-    pthread_create(&consumer_thread, nullptr, consumer, nullptr);
     pthread_create(&producer_thread, nullptr, producer, nullptr);
+    pthread_create(&consumer_thread, nullptr, consumer, nullptr);
 
     //ждем завершения
     pthread_join(producer_thread,nullptr);
     pthread_join(consumer_thread,nullptr);
 
-    // убиваем их
-    pthread_mutex_destroy(&monitor.mtx);
-    pthread_cond_destroy(&monitor.cv);
-
-    cout << "\nСпасибо за работу\n\n" << endl;
+    cout << "\nWork is done\n\n" << endl;
     return 0;
 }
